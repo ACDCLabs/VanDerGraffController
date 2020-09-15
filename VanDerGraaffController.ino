100c4a36e62e514158653c91e749dfcf67757be5
ecloud <info@ecloud-technologies.com>
2020-09-15 08:48:35 +0200

    tast check

--- VanDerGraaffController.ino
+++ VanDerGraaffController.ino
@@ -9,30 +9,35 @@ Servo ESC;     // create servo object to control the ESC
 
 int potValue;  // value from the analog pin
 
+#define UP_BUTTON 9
+#define DOWN_BUTTON 10
+
 void setup() {
 
-  
+  pinMode(UP_BUTTON, INPUT_PULLUP);
+  pinMode(DOWN_BUTTON, INPUT_PULLUP);
+
   Serial.begin(115200);
   // Attach the ESC on pin 9
-  ESC.attach(8,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
-  //ESC.attach(9); // (pin, min pulse width, max pulse width in microseconds) 
+  ESC.attach(8, 1000, 2000); // (pin, min pulse width, max pulse width in microseconds)
+  //ESC.attach(9); // (pin, min pulse width, max pulse width in microseconds)
   int i;
   ESC.write(90);  // neutral = motor stops used to arm ESC
   //ESC.writeMicroseconds(1000);
   delay(1000);
 
   /*
-  for ( i =0; i< 180; i++) {
+    for ( i =0; i< 180; i++) {
     ESC.write(i);
     delay(1000);
-  }
-  
-  
-  Serial.println(i);
-  
-   */
-   //ESC.write(40);  
-   //ESC.writeMicroseconds(2000);
+    }
+
+
+    Serial.println(i);
+
+  */
+  //ESC.write(40);
+  //ESC.writeMicroseconds(2000);
   //ESC.write(180);
   //delay(2000);
   //ESC.write(0);
@@ -41,58 +46,21 @@ void setup() {
 void loop() {
   // potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
   // potValue = map(potValue, 0, 1023, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
-    // Send the signal to the ESC
-ESC.write(90);
-delay(2000);
+  // Send the signal to the ESC
+  ESC.write(90);
+  delay(2000);
 
-int mySpeed=180;
-  //delay(100);
+  int speed = 0;
 
-Serial.println("Direction one - Accel"); 
-for (int s = 90; s<mySpeed; s++){
-    ESC.write(s);
-    Serial.println(s);
-    delay(100);
+  while (digitalRead(UP_BUTTON) == HIGH) {
+    if ( wait(1000)  == TRUE) {
+      speed ++;
+
+    }
+    Serial.println(speed);
   }
 
-Serial.println("Direction one - Decel");  
-for (int s = mySpeed; s>90; s--) {
-  ESC.write(s);
-  Serial.println(s);
-  delay(100);  
 }
-
-Serial.println("Stop");
-delay(2000);
-pinMode(9, INPUT_PULLUP);
-pinMode(10, INPUT_PULLUP);
-
-Serial.println("Direction two - Accel"); 
-
-for (int s = 90;s>0; s--){
-  ESC.write(s);
-  Serial.println(s);
-  delay(100);
-}
-
-
-Serial.println("Direction two - Decel"); 
-
-for (int s = 0;s<90; s++){
-  ESC.write(s);
-  Serial.println(s);
-  delay(100);
-}
-
-  
-Serial.println("Stop");
-delay(2000);
-
-  while(true ) ESC.write(90);
-  //ESC.writeMicroseconds(1500);
-
-}
-
 
 bool  wait(int millisWait) {
 
@@ -115,5 +83,56 @@ bool  wait(int millisWait) {
     return true;
   }
   else return false;
+
+}
+
+
+void moveMotor() {
+  int mySpeed = 180;
+  //delay(100);
+
+  Serial.println("Direction one - Accel");
+  for (int s = 90; s < mySpeed; s++) {
+    ESC.write(s);
+    Serial.println(s);
+    delay(100);
+  }
+
+  Serial.println("Direction one - Decel");
+  for (int s = mySpeed; s > 90; s--) {
+    ESC.write(s);
+    Serial.println(s);
+    delay(100);
+  }
+
+  Serial.println("Stop");
+  delay(2000);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+
+  Serial.println("Direction two - Accel");
+
+  for (int s = 90; s > 0; s--) {
+    ESC.write(s);
+    Serial.println(s);
+    delay(100);
+  }
+
+
+  Serial.println("Direction two - Decel");
+
+  for (int s = 0; s < 90; s++) {
+    ESC.write(s);
+    Serial.println(s);
+    delay(100);
+  }
+
+
+  Serial.println("Stop");
+  delay(2000);
+
+  while (true ) ESC.write(90);
+  //ESC.writeMicroseconds(1500);
+
 
 }
